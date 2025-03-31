60080362a2be729f649298e3bad20dbe34bcf92f
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-03-31 17:11:49 +0100

    fix: specify correct app name in build file

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py makemigrations blog
-python manage.py migrate blog 
+python manage.py makemigrations api
+python manage.py migrate api
 
 python manage.py makemigrations
 python manage.py migrate
