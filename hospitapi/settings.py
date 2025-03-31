d44f909d02b9bcd8f291ddd3bb4ede6395483bfe
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-03-31 18:11:40 +0100

    feat: reconfigure default css to be rendered

--- hospitapi/settings.py
+++ hospitapi/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = "hospitapi.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
